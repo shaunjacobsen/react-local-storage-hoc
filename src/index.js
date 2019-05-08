4b792bc4c9f579a285826485453944d6a10fff61
Shaun Jacobsen <sjacobsen@grubhub.com>
2019-05-08 09:57:21 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default (utiliserStockageLocal = WrapperComponent => {
+  class HOC extends React.Component {
+    state = {
+      stockageLocalDisponible: false,
+    };
+
+    componentDidMount() {
+      this.verifierDisponibiliteDuStockageLocal();
+    }
+
+    verifierDisponibiliteDuStockageLocal() {
+      const essai = 'essai';
+
+      try {
+        localStorage.setItem(essai, essai);
+        localStorage.removeItem(essai);
+        this.setState({ stockageLocalDisponible: true });
+      } catch (e) {
+        this.setState({ stockageLocalDisponible: false });
+      }
+    }
+
+    charger = cle => {
+      if (this.state.stockageLocalDisponible) {
+        return localStorage.getItem(cle);
+      }
+
+      return undefined;
+    };
+
+    sauvegarder = (cle, infos) => {
+      this.state.stockageLocalDisponible && localStorage.setItem(cle, infos);
+    };
+
+    supprimer = cle => {
+      this.state.stockageLocalDisponible && localStorage.removeItem(cle);
+    };
+
+    render() {
+      return (
+        <WrapperComponent
+          load={this.charger}
+          charger={this.charger}
+          save={this.sauvegarder}
+          sauvegarder={this.sauvegarder}
+          remove={this.supprimer}
+          supprimer={this.supprimer}
+          {...this.props}
+        />
+      );
+    }
+  }
+
+  return HOC;
+});
